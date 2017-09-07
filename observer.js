612271d737e0c9b845ec2d3bc494f593ea71a7c4
Aleksandr Denisenko <abyrcheg@gmail.com>
2017-09-07 09:22:29 +0000

    using Observer instance

--- observer.js
+++ observer.js
@@ -1,109 +1,109 @@
 (function () {
-    var lastUrl = window.location.href;
-
-    var config = window.website_observer_config || { pageview: { onload: true } };
-
-    var triggerEvent = function (name, type) {
-            /* global CustomEvent */
-            var event;
-
-            if (window.CustomEvent) {
-                event = new CustomEvent('wso-event', {detail: {name: name, type: type}});
-            } else {
-                event = document.createEvent('CustomEvent');
-                event.initCustomEvent('wso-event', true, true, {name: name, type: type});
-            }
-
-            document.dispatchEvent(event);
-        };
-
-    var countEvent = function (name, type) {
-            if (typeof eventsHistory[name] !== 'undefined' &&
-                typeof eventsHistory[name][type] !== 'undefined') {
-                eventsHistory[name][type].push(true);
-                console.log('Event', name, type, 'count:', eventsHistory[name][type].length, eventsHistory);
-                triggerEvent(name, type);
-            } else {
-                console.log('Undefined event', name, type);
-            }
-            debug && updateDebugIframe();
-        };
-
-    var eventListeners = {
-            urlChanged: function (urlPattern) {
-                if (lastUrl !== window.location.href) {
-                    lastUrl = window.location.href;
-
-                    if (config.pageview.url_change) {
-                        countEvent('pageview', 'url_change');
-                    }
-
-                    if (urlPattern && !!lastUrl.match(urlPattern)) {
-                        countEvent('pageview', 'url_match');
-                    }
-                }
-            }
-        };
-
-    var eventsHistory = {
-            pageview: {}
-        };
-
-    var printableEventsHistory = function () {
-            var res = {},
-                configuredEvents = window.website_observer_config;
-
-            Object.keys(configuredEvents).forEach(function (eventName, index) {
-                res[eventName] = {};
-                Object.keys(configuredEvents[eventName]).forEach(function (eventType, index) {
-                    if (typeof eventsHistory[eventName][eventType] !== 'undefined') {
-                        res[eventName][eventType] = eventsHistory[eventName][eventType].length;
-                    }
-                });
-            });
-            return JSON.stringify(res);
-        };
-
-    var debug = true,
-        debugIframe;
+    var eventsConfig = window.website_observer_config || { pageload: true, url_change: true },
+        debug = 1,
+        debugIframe,
+        alleventsList = ['pageload', 'url_change', 'url_change_match', 'click'],
+        observer;
 
     var onReady = function (fn) {
-        if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
-            fn();
-        } else {
-            document.addEventListener('DOMContentLoaded', fn);
+            if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
+                fn();
+            } else {
+                document.addEventListener('DOMContentLoaded', fn);
+            }
+        },
+        updateDebugIframe = function () {
+            if (debugIframe) {
+                debugIframe.contentDocument.body.innerHTML = observer.printableEventsHistory();
+            }
+        };
+
+    var Observer = function (config, options) {
+            this.eventsConfig = config || {};
+            this.lastUrl = window.location.href;
+            this.eventsList = alleventsList,
+            this.eventsHistory = {};
+            this.eventsList.forEach(function (eventName) {
+                this.eventsHistory[eventName] = [];
+            }.bind(this));
+        };
+
+    Observer.prototype.start = function () {
+        this.fireEvent('pageload');
+        this.observeUrlChange(this.eventsConfig.url_change_match);
+
+        if (debug) {
+            debugIframe = document.createElement('iframe');
+            document.body.appendChild(debugIframe);
+            debugIframe.contentDocument.body.innerHTML = observer.printableEventsHistory();
         }
     };
 
-    debug && onReady(function () {
-        debugIframe = document.createElement('iframe');
-        document.body.appendChild(debugIframe);
-        debugIframe.contentDocument.body.innerHTML = printableEventsHistory();
-    });
+    Observer.prototype.printableEventsHistory = function () {
+        var res = {};
 
-    var updateDebugIframe = function () {
-            if (debugIframe) {
-                debugIframe.contentDocument.body.innerHTML = printableEventsHistory();
+        Object.keys(this.eventsConfig).forEach(function (eventName, index) {
+            if (typeof this.eventsHistory[eventName] !== 'undefined') {
+                res[eventName] = this.eventsHistory[eventName].length;
             }
-        };
+        }.bind(this));
+        return JSON.stringify(res);
+    };
 
-    // todo: init configured keys
-    eventsHistory['pageview'] = {};
-
-    if (config.pageview.onload) {
-        eventsHistory['pageview']['onload'] = [];
-        countEvent('pageview', 'onload');
-    }
-
-    if (config.pageview.url_change || config.pageview.url_match) {
-        if (config.pageview.url_change) {
-            eventsHistory['pageview']['url_change'] = [];
-        }
-        if (config.pageview.url_match) {
-            eventsHistory['pageview']['url_match'] = [];
-        }
+    Observer.prototype.observeUrlChange = function (urlPattern) {
         setInterval(function () {
-            eventListeners.urlChanged(config.pageview.url_match);
-        }, 100);
-    }
+            this.isUrlChanged(urlPattern);
+        }.bind(this), 100);
+    };
+
+    /**
+     * @private
+     */
+    Observer.prototype.isUrlChanged = function (urlPattern) {
+        if (this.lastUrl === window.location.href) {
+            return;
+        }
+        this.lastUrl = window.location.href;
+        if (this.eventsConfig.url_change) {
+            this.fireEvent('url_change');
+        }
+        if (urlPattern && !!this.lastUrl.match(urlPattern)) {
+            this.fireEvent('url_change_match');
+        }
+    };
+
+    Observer.prototype.clickListener = function () {
+
+    };
+
+    /**
+     * @private
+     */
+    Observer.prototype.triggerEvent = function (eventName) {
+        /* global CustomEvent */
+        var observerEvent;
+
+        if (window.CustomEvent) {
+            observerEvent = new CustomEvent('wso-event', {detail: {name: eventName}});
+        } else {
+            observerEvent = document.createEvent('CustomEvent');
+            observerEvent.initCustomEvent('wso-event', true, true, {name: eventName});
+        }
+        document.dispatchEvent(observerEvent);
+    };
+
+    /**
+     * @private
+     */
+    Observer.prototype.fireEvent = function (eventName) {
+        this.eventsHistory[eventName].push(true);
+        if (debug) {
+            updateDebugIframe();
+        }
+        this.triggerEvent(eventName);
+    };
+
+    observer = new Observer(eventsConfig);
+    onReady(function () { observer.start(); });
+
 })();
